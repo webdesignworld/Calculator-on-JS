a55d227b751c9d66b6faba3ddda2e446e6411f90
Svetlana <72972093+webdesignworld@users.noreply.github.com>
2022-12-10 16:35:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//1. created all buttons 
+ 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -8,7 +8,7 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
 
-//2. class with constructor for placement of info on display
+class with constructor for placement of info on display
 class Calculator {
         constructor(previousOperandTextElement, currentOperandTextElement) {
           this.previousOperandTextElement = previousOperandTextElement;
